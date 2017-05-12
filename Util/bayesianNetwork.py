f37dcb402e9ba816ffbe50d5c7494e16a6661bd5
afcajamarcar <afcajamarcar@unal.edu.co>
2017-05-11 19:01:33 -0500

    Added centroid method

--- Util/bayesianNetwork.py
+++ Util/bayesianNetwork.py
@@ -9,6 +9,7 @@
 ########################################
 from resize import resizeSignature
 import matplotlib.pyplot as plt
+import numpy as np
 
 def filter(grayScaleSignature):
     filteredSignature = grayScaleSignature < 128
@@ -16,6 +17,21 @@ def filter(grayScaleSignature):
 
 img = filter(resizeSignature('Signature.jpg'))
 
+def centroid(img):
+    ones = []
+    for x in xrange(len(img)):
+        for y in xrange(len(img[0])):
+                if img[x,y] == 1:
+                    ones.append((x,y))
+    sumx = 0
+    sumy = 0
+    for item in ones:
+        sumy+=item[0]
+        sumx+=item[1]
+    meanx = sumx/len(ones)
+    meany = sumy/len(ones)
+    return (meanx, meany)
+print centroid(img)
 plt.imshow(img)
 
 plt.show()
