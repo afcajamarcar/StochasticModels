ef2c7665d4bd24f62bd9e7cfcb016e203ce6a0a4
japrietov <japrietov@unal.edu.co>
2017-05-16 10:39:17 -0500

    Skew detection - Angle signature

--- Util/bayesianNetwork.py
+++ Util/bayesianNetwork.py
@@ -11,6 +11,7 @@ from resize import resizeSignature
 from gaussianSmoothing import gaussianBlur
 import matplotlib.pyplot as plt
 import numpy as np
+import operator
 from sklearn.cluster import KMeans
 from scipy.stats import kurtosis
 
@@ -18,8 +19,6 @@ from scipy.stats import kurtosis
 def filter(grayScaleSignature):
     filteredSignature = grayScaleSignature < 64
     return filteredSignature.astype(int)
-
-img = resizeSignature('Signature_2.jpg')
 
 def calculateBlackPoints(img):
     ones = []
@@ -29,55 +28,94 @@ def calculateBlackPoints(img):
                 ones.append((x, y))
     return ones
 
-
-
-
-def centroid(img):
-    ones = calculateBlackPoints(img)
+def centroid(blackPointsImg):
     sumx = 0
     sumy = 0
-    for item in ones:
+    for item in blackPointsImg:
         sumy += item[0]
         sumx += item[1]
-    meanx = sumx/float(len(ones))
-    meany = sumy/float(len(ones))
+    meanx = sumx/float(len(blackPointsImg))
+    meany = sumy/float(len(blackPointsImg))
 
     return (round(meanx), round(meany))
 
-def calculateDensePoints(img, numberDensePoints=30):
-    positions = calculateBlackPoints(img)
+def calculateDensePoints(blackPointsImg, numberDensePoints=30):
+    positions = blackPointsImg
     kmeansDensePoints = KMeans(n_clusters=numberDensePoints, random_state=0, n_jobs=-1).fit(positions)
     return kmeansDensePoints.cluster_centers_
+
 
 #Calculates Fisher Kurtosis by default, returns an array (it could return Pearson's measure of kurtosis)
 def calculateKurtosis(img):
     return kurtosis(img)
 
 
-# Image filtering
-imgSmooted = gaussianBlur(2,3,img)
-imgFiltered = filter(imgSmooted)
+def skewDetection(img):
+    middleLeft = img[ : , :len(img)/2]
+    middleRight = img[ : , (len(img)/2)+1:]
 
-# Calculate the ones in the matrix.
-some = calculateBlackPoints(imgFiltered)
+    print middleLeft.shape, middleRight.shape
 
-# Just for print the matrix
-list1= [ x[0] for x in some ]
-list2= [ x[1] for x in some ]
-plt.plot(list1, list2, "ro")
+    centerMassLeft = centroid(calculateBlackPoints(middleLeft))
+    centerMassRight = centroid(calculateBlackPoints(middleRight))
 
-# Calculate the representative points of a signature
-densePoints = calculateDensePoints(imgFiltered)
+    # fix center mass right
+    centerMassRight = tuple(map(operator.add, centerMassRight, (250, 0)))
+
+    def angle_between(p1, p2):
+        ang1 = np.arctan2(*p1[::-1])
+        ang2 = np.arctan2(*p2[::-1])
+        return np.rad2deg((ang1 - ang2) % (2 * np.pi))
+
+    return angle_between(centerMassLeft, centerMassRight)
 
 
-print calculateKurtosis(imgFiltered)
-# For print in better way
-# densePoints[:,[0, 1]] = densePoints[:,[1, 0]]
-# plt.plot(list1, list2, "ro")
-densePoints = densePoints.T
-plt.plot(densePoints[0],densePoints[1], "+")
+if __name__ == "__main__":
 
-plt.show()
+    img = resizeSignature('Signature.jpg')
+    # Image filtering
+    imgSmooted = gaussianBlur(2, 1, img)
+    imgFiltered = filter(imgSmooted)
+
+
+    # Calculate the ones in the matrix.
+    blackPoints = calculateBlackPoints(imgFiltered)
+
+    # Calculate the center of mass
+    print blackPoints
+    centerMass = centroid(blackPoints)
+
+    print centerMass
+
+    # Calculate the representative points of a signature
+    densePoints = calculateDensePoints(blackPoints)
+
+    # For print in better way
+    # densePoints[:,[0, 1]] = densePoints[:,[1, 0]]
+    # plt.plot(list1, list2, "ro")
+    # Just for print the matrix
+    list1 = [x[0] for x in blackPoints]
+    list2 = [x[1] for x in blackPoints]
+    plt.plot(list1, list2, "ro")
+
+    # Print Center of mass
+    plt.plot(centerMass[1], centerMass[0], "^")
+
+    # Print dense points
+    densePoints = densePoints.T
+    plt.plot(densePoints[0], densePoints[1], "g+")
+
+    print calculateKurtosis(imgFiltered)
+
+    # Print skew detection
+    testSkew = skewDetection(imgFiltered)
+
+    # plt.plot([y positions of the points], [x positions of the points])
+    #plt.plot([testSkewLeft[1], testSkewRight[1]], [testSkewLeft[0], testSkewRight[0]], 'k')
+    # plt.plot([testSkewLeft[1], testSkewLeft[1]], [testSkewLeft[0], testSkewRight[0]], 'k')
+    print "Angle signature", testSkew
+
+    plt.show()
 
 
 
