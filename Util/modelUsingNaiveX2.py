511fd9f2533a0804bca2382680016b364b83fa65
japrietov <japrietov@unal.edu.co>
2017-06-01 18:53:37 -0500

    Learning information for each signature

--- Util/modelUsingNaiveX2.py
+++ Util/modelUsingNaiveX2.py
@@ -9,15 +9,18 @@ from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 import csv
 import random
 
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+
 import numpy as np
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score
-
-
-
 from sklearn.base import BaseEstimator, ClassifierMixin
+
 
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
@@ -43,21 +46,46 @@ class TemplateClassifier(BaseEstimator, ClassifierMixin):
 
     def predict(self, X):
 
+        def hibridModel(x):
+            naiveClassifier = GaussianNB().fit(self.X_, self.y_)
+            y_predict_Naive = naiveClassifier.predict(x)[0]
+
+
+            matrixDataTmp = []
+            vectorTargetTmp = []
+            with open('../LearningInformation/learningBayesianBinomial'+str(y_predict_Naive) + '.csv') as f:
+                readerGenuine = csv.reader(f)
+
+                readerGenuine.next()
+                for i in readerGenuine:
+                    matrixDataTmp.append(map(float, i[:len(i) - 1]))
+                    # matrixData.append(map(float, i[2:5] + i[25:len(i) - 1]))
+                    vectorTargetTmp.append(int(i[len(i) - 1]))
+
+            secondClassifier = svm.SVC(kernel='linear', C=0.8).fit(matrixDataTmp, vectorTargetTmp)
+            prediction = secondClassifier.predict(x)[0]
+            if prediction == 1:
+                return y_predict_Naive
+            else:
+                return 0
         # Check is fit had been called
         check_is_fitted(self, ['X_', 'y_'])
 
         # Input validation
         X = check_array(X)
 
+        y_predicted = [hibridModel(x) for x in X]
+        return y_predicted
 
-        closest = np.argmin(euclidean_distances(X, self.X_), axis=1)
-        print closest
-        return self.y_[closest]
+
+def split_train(X, y, test_size=0.2):
+    index = random.sample(range(len(X)), int(len(X)*test_size))
+    return index
 
 
 matrixData = []
 vectorTarget = []
-
+realVectorTarget = []
 with open('../LearningInformation/learningBayesianMulticlassGenuine.csv') as f:
     readerGenuine = csv.reader(f)
 
@@ -66,38 +94,45 @@ with open('../LearningInformation/learningBayesianMulticlassGenuine.csv') as f:
         matrixData.append(map(float,  i[:len(i)-1]))
         #matrixData.append(map(float, i[2:5] + i[25:len(i) - 1]))
         vectorTarget.append(int(i[len(i)-1]))
+        realVectorTarget.append(int(i[len(i)-1]))
 
-readerForgeries = list()
 with open('../LearningInformation/learningBayesianMulticlassForgeries.csv') as f:
     readerForgeriestmp = csv.reader(f)
     readerForgeriestmp.next()
-    for i in readerGenuine:
+    for i in readerForgeriestmp:
         matrixData.append(map(float,  i[:len(i)-1]))
         #matrixData.append(map(float, i[2:5] + i[25:len(i) - 1]))
         vectorTarget.append(int(i[len(i)-1]))
+        realVectorTarget.append(0)
 
 matrixData = np.array(matrixData)
 vectorTarget = np.array(vectorTarget)
+scores = []
 
-X_train, X_test, y_train, y_test = train_test_split(matrixData, vectorTarget, test_size=0.2, random_state=0)
+for i in range(10):
+    X_train, X_test, y_train, y_test, y_realTest = [],[],[],[],[]
 
-asdsada = TemplateClassifier().fit(X_train, y_train)
-print asdsada
-y_predict = asdsada.predict(X_test)
-
-print accuracy_score(y_test, y_predict)
-
-###################### Naive Bayes
-naiveClassifier = GaussianNB().fit(X_train, y_train)
-y_predict_NaiveProba = naiveClassifier.predict_proba(X_test)
-y_predict_Naive = naiveClassifier.predict(X_test)
-
-#print accuracy_score(y_test, y_predict_Naive)
+    indexTest = set(split_train(matrixData, vectorTarget))
 
 
-######## Crossvalidation
-#scores = cross_val_score(asdsada, matrixData, vectorTarget, cv=5)
-#print("Accuracy SVM: %0.4f (+/- %0.4f)" % (scores.mean(), scores.std() * 2))
+    for index in range(len(matrixData)):
+        if index in indexTest:
+            X_test.append(matrixData[index])
+            y_test.append(vectorTarget[index])
+            y_realTest.append(realVectorTarget[index])
+        else:
+            X_train.append(matrixData[index])
+            y_train.append(vectorTarget[index])
+
+    asdsada = TemplateClassifier().fit(X_train, y_train)
+    print asdsada
+    y_predict = asdsada.predict(X_test)
+
+    scores.append(accuracy_score(y_realTest, y_predict))
+
+    #print accuracy_score(y_test, y_predict_Naive)
+
+print("Accuracy SVM: %0.4f (+/- %0.4f)" % (np.array(scores).mean(), np.array(scores).std() * 2))
 
 #scores = cross_val_score(naiveClassifier, matrixData, vectorTarget, cv=5)
 #print("Accuracy Naive: %0.4f (+/- %0.4f)" % (scores.mean(), scores.std() * 2))
