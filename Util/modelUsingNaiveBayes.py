c1f1b2743bcf871e31fc3b8a54329d3e48dc78ec
japrietov <japrietov@unal.edu.co>
2017-06-03 22:43:19 -0500

    Learning information for each signature

--- Util/modelUsingNaiveBayes.py
+++ Util/modelUsingNaiveBayes.py
@@ -1,5 +1,3 @@
-########################################
-# Universidad Nacional de Colombia
 # Sthochastic Models
 ########################################
 # Authors
@@ -11,11 +9,26 @@ from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 import csv
 import random
 
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+
 import numpy as np
 from sklearn import svm
+
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.utils.multiclass import unique_labels
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+
 
 
 
@@ -47,29 +60,92 @@ for forgSing in readerForgeries:
 matrixData = np.array(matrixData)
 vectorTarget = np.array(vectorTarget)
 
-##### Support Vector machine
-asdsada = svm.SVC(kernel='linear', C=1)
-X_train, X_test, y_train, y_test = train_test_split(matrixData, vectorTarget, test_size=0.2, random_state=0)
-asdsada.fit(X_train, y_train)
-y_predict = asdsada.predict(X_test)
+totalScores = []
+totalF1Scores = []
 
-print asdsada.score(X_test, y_test)
-print accuracy_score(y_test, y_predict)
+for j in range(10):
+    # Cross validation
+    scores = []
+    f1Scores = []
+
+    ##### Cross Validation
+    for i in range(5):
+        X_train, X_test, y_train, y_test = train_test_split(matrixData, vectorTarget, test_size=0.2,
+                                                            random_state=random.randint(1, 10))
+
+        ###################### Naive Bayes
+        naiveClassifier = GaussianNB().fit(X_train, y_train)
+        y_predict_Naive = naiveClassifier.predict(X_test)
+
+        scores.append(accuracy_score(y_test, y_predict_Naive))
+        f1Scores.append(f1_score(y_test, y_predict_Naive, average='weighted'))
+
+    print("Accuracy Naive: %0.4f (+/- %0.4f)" % (np.array(scores).mean(), np.array(scores).std() * 2))
+    print("F1 Naive: %0.4f (+/- %0.4f)" % (np.array(f1Scores).mean(), np.array(f1Scores).std() * 2))
+
+    totalF1Scores.append(np.array(f1Scores).mean())
+    totalScores.append(np.array(scores).mean())
+
+print
+print
+print("Accuracy Naive: %0.4f (+/- %0.4f)" % (np.array(scores).mean(), np.array(scores).std() * 2))
+print("F1 Naive: %0.4f (+/- %0.4f)" % (np.array(f1Scores).mean(), np.array(f1Scores).std() * 2))
 
 
-###################### Naive Bayes
-naiveClassifier = GaussianNB().fit(X_train, y_train)
-y_predict_Naive = naiveClassifier.predict(X_test)
 
 
-print y_predict_Naive
-print y_test
-print accuracy_score(y_test, y_predict_Naive)
 
 
-######## Crossvalidation
-scores = cross_val_score(asdsada, matrixData, vectorTarget, cv=5)
-print("Accuracy SVM: %0.4f (+/- %0.4f)" % (scores.mean(), scores.std() * 2))
 
-scores = cross_val_score(naiveClassifier, matrixData, vectorTarget, cv=5)
-print("Accuracy Naive: %0.4f (+/- %0.4f)" % (scores.mean(), scores.std() * 2))
+########################################################
+# Plot the confusion matrix
+########################################################
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        cm = np.around(cm, decimals=2)
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.colorbar()
+
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, y_predict_Naive)
+np.set_printoptions(precision=2)
+
+class_names = ['00','01','02','03','04','06','09','12','14','15','16']
+
+# Plot normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(np.around(cnf_matrix, decimals=3), classes=class_names, normalize=True,
+                      title='Normalized confusion matrix')
+# plt.show()
+
+#scores = cross_val_score(naiveClassifier, matrixData, vectorTarget, cv=5)
+#print("Accuracy Naive: %0.4f (+/- %0.4f)" % (scores.mean(), scores.std() * 2))
+
+
--- Util/modelUsingNaiveX2.py
+++ Util/modelUsingNaiveX2.py
@@ -16,17 +16,21 @@ warnings.warn = warn
 
 import numpy as np
 from sklearn import svm
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import accuracy_score
-from sklearn.base import BaseEstimator, ClassifierMixin
 
-
-import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import unique_labels
-from sklearn.metrics import euclidean_distances
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import f1_score
+
+
+
+
 class TemplateClassifier(BaseEstimator, ClassifierMixin):
 
     def __init__(self, demo_param='demo'):
@@ -62,7 +66,7 @@ class TemplateClassifier(BaseEstimator, ClassifierMixin):
                     # matrixData.append(map(float, i[2:5] + i[25:len(i) - 1]))
                     vectorTargetTmp.append(int(i[len(i) - 1]))
 
-            secondClassifier = svm.SVC(kernel='linear', C=0.8).fit(matrixDataTmp, vectorTargetTmp)
+            secondClassifier = svm.SVC(kernel='linear', C=0.2).fit(matrixDataTmp, vectorTargetTmp)
             prediction = secondClassifier.predict(x)[0]
             if prediction == 1:
                 return y_predict_Naive
@@ -107,32 +111,117 @@ with open('../LearningInformation/learningBayesianMulticlassForgeries.csv') as f
 
 matrixData = np.array(matrixData)
 vectorTarget = np.array(vectorTarget)
-scores = []
 
-for i in range(10):
-    X_train, X_test, y_train, y_test, y_realTest = [],[],[],[],[]
-
-    indexTest = set(split_train(matrixData, vectorTarget))
+totalScores = []
+totalF1Scores = []
 
 
-    for index in range(len(matrixData)):
-        if index in indexTest:
-            X_test.append(matrixData[index])
-            y_test.append(vectorTarget[index])
-            y_realTest.append(realVectorTarget[index])
-        else:
-            X_train.append(matrixData[index])
-            y_train.append(vectorTarget[index])
+y_predict_best = []
+accuracyFlag = 0
 
-    asdsada = TemplateClassifier().fit(X_train, y_train)
-    print asdsada
-    y_predict = asdsada.predict(X_test)
 
-    scores.append(accuracy_score(y_realTest, y_predict))
+for j in range(5):
+    # Cross validation
+    scores = []
+    f1Scores = []
 
-    #print accuracy_score(y_test, y_predict_Naive)
+    y_predictB = []
+    accuracyTmp11 = 0
+    for i in range(5):
+        X_train, X_test, y_train, y_test, y_realTest = [],[],[],[],[]
 
-print("Accuracy SVM: %0.4f (+/- %0.4f)" % (np.array(scores).mean(), np.array(scores).std() * 2))
+        indexTest = set(split_train(matrixData, vectorTarget))
+
+
+        for index in range(len(matrixData)):
+            if index in indexTest:
+                X_test.append(matrixData[index])
+                y_test.append(vectorTarget[index])
+                y_realTest.append(realVectorTarget[index])
+            else:
+                X_train.append(matrixData[index])
+                y_train.append(vectorTarget[index])
+
+        asdsada = TemplateClassifier().fit(X_train, y_train)
+        y_predict = asdsada.predict(X_test)
+
+
+        accuracyTmp = accuracy_score(y_realTest, y_predict)
+        if accuracyTmp > accuracyTmp11:
+            accuracyTmp11 = accuracyTmp
+            y_predictB = y_predict
+
+        scores.append(accuracy_score(y_realTest, y_predict))
+        f1Scores.append(f1_score(y_realTest, y_predict, average='weighted'))
+
+    accuracyCurrent = np.array(scores).mean()
+
+    if accuracyCurrent > accuracyFlag:
+        accuracyFlag = accuracyCurrent
+        y_predict_best = y_predictB
+
+    print("Accuracy Naive: %0.4f (+/- %0.4f)" % (accuracyCurrent, np.array(scores).std() * 2))
+    print("F1 Naive: %0.4f (+/- %0.4f)" % (np.array(f1Scores).mean(), np.array(f1Scores).std() * 2))
+
+    totalF1Scores.append(np.array(f1Scores).mean())
+    totalScores.append(accuracyCurrent)
+
+print
+print
+print("Accuracy Naive: %0.4f (+/- %0.4f)" % (np.array(scores).mean(), np.array(scores).std() * 2))
+print("F1 Naive: %0.4f (+/- %0.4f)" % (np.array(f1Scores).mean(), np.array(f1Scores).std() * 2))
+
+
+print y_predict_best
+print y_realTest
+
+########################################################
+# Plot the confusion matrix
+########################################################
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        cm = np.around(cm, decimals=2)
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.colorbar()
+
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_realTest, y_predict_best)
+np.set_printoptions(precision=2)
+
+class_names = ['00','01','02','03','04','06','09','12','14','15','16']
+
+# Plot normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(np.around(cnf_matrix, decimals=3), classes=class_names, normalize=True,
+                      title='Normalized confusion matrix')
+plt.show()
 
 #scores = cross_val_score(naiveClassifier, matrixData, vectorTarget, cv=5)
 #print("Accuracy Naive: %0.4f (+/- %0.4f)" % (scores.mean(), scores.std() * 2))
